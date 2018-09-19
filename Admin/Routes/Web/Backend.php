d9fe4628f011460dbd0205bfab3ffd3248d6465b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-09-19 21:34:04 +0200

    Split controllers per application

--- Admin/Routes/Web/Backend.php
+++ Admin/Routes/Web/Backend.php
@@ -3,15 +3,15 @@
 use phpOMS\Router\RouteVerb;
 use phpOMS\Account\PermissionType;
 use Modules\ContractLifecycleManagement\Models\PermissionState;
-use Modules\ContractLifecycleManagement\Controller;
+use Modules\ContractLifecycleManagement\Controller\BackendController;
 
 return [
     '^.*/backend/controlling/clm/dashboard.*$' => [
         [
-            'dest' => '\Modules\ContractLifecycleManagement\Controller:viewContractDashboard',
+            'dest' => '\Modules\ContractLifecycleManagement\Controller\BackendController:viewContractDashboard',
             'verb' => RouteVerb::GET,
             'permission' => [
-                'module' => Controller::MODULE_NAME,
+                'module' => BackendController::MODULE_NAME,
                 'type'  => PermissionType::READ,
                 'state' => PermissionState::CLM,
             ],
