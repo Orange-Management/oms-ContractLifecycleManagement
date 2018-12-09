2723638c8ea582571080600ed86f1e0dfbb148c4
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-12-09 22:16:55 +0100

    Fix use order

--- Admin/Routes/Web/Backend.php
+++ Admin/Routes/Web/Backend.php
@@ -1,9 +1,9 @@
 <?php
 
-use phpOMS\Router\RouteVerb;
-use phpOMS\Account\PermissionType;
-use Modules\ContractLifecycleManagement\Models\PermissionState;
 use Modules\ContractLifecycleManagement\Controller\BackendController;
+use Modules\ContractLifecycleManagement\Models\PermissionState;
+use phpOMS\Account\PermissionType;
+use phpOMS\Router\RouteVerb;
 
 return [
     '^.*/backend/controlling/clm/dashboard.*$' => [
