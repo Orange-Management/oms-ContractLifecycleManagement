409041feaab285b8f5a1f63675fc070ade46a644
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-03-09 23:21:29 +0100

    Make app,localization in url optional+tld+subdomain

--- Admin/Routes/Web/Backend.php
+++ Admin/Routes/Web/Backend.php
@@ -6,7 +6,7 @@ use phpOMS\Account\PermissionType;
 use phpOMS\Router\RouteVerb;
 
 return [
-    '^.*/backend/controlling/clm/dashboard.*$' => [
+    '^.*/controlling/clm/dashboard.*$' => [
         [
             'dest' => '\Modules\ContractLifecycleManagement\Controller\BackendController:viewContractDashboard',
             'verb' => RouteVerb::GET,
