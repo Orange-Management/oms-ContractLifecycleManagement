e70b0feb34e5fc25a0bed7dffcef8600fc818642
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-02-06 23:54:01 +0100

    improve baseline

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -43,7 +43,7 @@ final class BackendController extends Controller
      */
     public function viewContractDashboard(RequestAbstract $request, ResponseAbstract $response, $data = null) : RenderableInterface
     {
-        $view = new View($this->app, $request, $response);
+        $view = new View($this->app->l11nManager, $request, $response);
         $view->setTemplate('/Modules/ContractLifecycleManagement/Theme/Backend/contract-dashboard');
         $view->addData('nav', $this->app->moduleManager->get('Navigation')->createNavigationMid(1004601001, $request, $response));
 
